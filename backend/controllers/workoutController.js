23945ecadb453559cbfa70f597eed6cbc3577c56
Hüveyda Tükle <huveydatukle@Huveydas-MacBook-Air-2.local>
2023-09-12 16:51:28 +0100

    created controller part2 update and delete method

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -34,12 +34,48 @@ const createWorkout = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+
 //delete a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  const workout = await Workout.findByIdAndDelete({ _id: id });
+  if (!workout) {
+    return res.status(400).json({ error: "No such workout" });
+  }
+  res.status(200).json(workout);
+};
 
 // update a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  const workout = await Workout.findByIdAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!workout) {
+    return res.status(400).json({ error: "No such workout" });
+  }
+
+  res.status(200).json({ workout });
+};
 
 module.exports = {
   createWorkout,
   getWorkout,
   getWorkouts,
+  deleteWorkout,
+  updateWorkout,
 };
