0a92c8a64f432873ec2171139d0079dea6d81451
Hüveyda Tükle <huveydatukle@Huveydas-MacBook-Air-2.local>
2023-09-13 12:29:55 +0100

    fetching data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,35 @@
-import React from "react";
+import { useEffect, useState } from "react";
+
+//components
+import WorkoutDetails from "../components/WorkoutDetails";
 
 function Home() {
-  return <div className="home">Home</div>;
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      console.log("resp: ", json);
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
